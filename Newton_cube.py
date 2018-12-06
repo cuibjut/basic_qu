de9a1fc59cdc2d17cc5bd197297237c120dbce3d
cui_tizi <tizi@ibantang.com>
2018-12-06 11:22:57 +0800

    Newton's method iterates to find the cubic root

---
+++ Newton_cube.py
@@ -0,0 +1,19 @@
+#!coding=UTF_8
+import math
+
+
+def cube_compution(input):
+    x0 = 1
+    x1 = x0 - (x0 - input / math.pow(x0, 2)) / 3
+
+    while math.fabs(x1 - x0) > 0.000001:  # 一定要用fabs绝对值， 否则得到结果可能不准确
+        x0 = x1
+        x1 = x0 - (x0 - input / math.pow(x0, 2)) / 3
+
+    return x1
+
+
+if __name__ == '__main__':
+    input = 8
+    cube_value = cube_compution(input)
+    print(cube_value)
